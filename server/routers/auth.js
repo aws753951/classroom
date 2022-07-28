1676de4241f1a140e0342c0bdad5feb23c8c5cf6
Marc <a0983204766@gmail.com>
2022-07-29 00:27:09 +0800

    add register router

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 const User = require("../models").UserModel;
+const registerValidation = require("../validation").registerValidation;
+const loginValidation = require("../validation").loginValidation;
 
-router.post("/register", (req, res) => {});
+router.post("/register", async (req, res) => {
+  let { username, email, password, role } = req.body;
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let foundUser = await User.findOne({ email });
+  if (foundUser) return res.status(400).send("Email has been registered");
+  let newUser = new User(req.body);
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).send("Sccuessfully register this account.");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send("Account not saved, please contact author.");
+    });
+});
 router.post("/login", (req, res) => {});
 
 module.exports = router;
