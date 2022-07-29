58e3aade636e026e10e7d1435d5866a6e889413d
Marc <a0983204766@gmail.com>
2022-07-29 10:10:52 +0800

    add simple post course route

--- server/routers/course.js
+++ server/routers/course.js
@@ -1,9 +1,31 @@
 const router = require("express").Router();
 const Course = require("../models").CourseModel;
+const courseValidation = require("../validation").courseValidation;
 
 // create
 // post a new course
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  if (req.user.isStudent())
+    return res.status(400).send("Only instructor can post");
+  // need check course schema
+  let { title, description, price } = req.body;
+  let newCourse = new Course({
+    title,
+    description,
+    price,
+    instructor: req.user._id,
+  });
+  newCourse
+    .save()
+    .then(() => {
+      res.status(200).send("New course has been saved.");
+    })
+    .catch(() => {
+      res.status(400).send("Cant save course.");
+    });
+});
 // enroll a new course - student
 router.post("/enroll/:_id", (req, res) => {});
 
